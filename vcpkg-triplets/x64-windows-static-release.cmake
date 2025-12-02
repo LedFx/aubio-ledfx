bd787b44caf7df336a8052edefaa626a94d27566
Shaun Eccles-Smith <shauneccles@gmail.com>
2025-12-02 13:50:43 +1100

    Refactor vcpkg triplet files for static linking and release-only builds on Windows; update comments for clarity and remove unnecessary lines.

--- vcpkg-triplets/x64-windows-static-release.cmake
+++ vcpkg-triplets/x64-windows-static-release.cmake
@@ -5,7 +5,5 @@ set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 
-set(VCPKG_CMAKE_SYSTEM_NAME Windows)
-
 # Release-only builds - no debug libraries
 set(VCPKG_BUILD_TYPE release)
